44270fb8fcd5f7e1d48e8fd20306c76d744ae675
Soumyajit Samanta <samanta.soumyajit@gmail.com>
2022-11-05 14:21:29 +0530

    Added setup environment script for new pc.

---
+++ use.sh
@@ -0,0 +1,59 @@
+# Contains the paths defined and how to use paths
+
+# Parameters defined with P suffix
+CONTAINERDP=
+GRADLE_HOME_7P=
+JAVA_HOME_JDK17P=
+K3SP=
+MVN_HOMEP=
+MYSQLP=
+NODE_HOMEP=
+SPARKP=
+SQLITEP=
+
+
+
+echo "These are the paths you can add to environments:"
+# echo "0"
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "A"
+# echo "B"
+# echo "C"
+# echo "D"
+# echo "E"
+# echo "F"
+# echo "G"
+# echo "H"
+# echo "I"
+echo "JAVA_HOME_JDK17="
+# echo "K"
+# echo "L"
+# echo "M"
+# echo "N"
+# echo "O"
+# echo "P"
+# echo "Q"
+# echo "R"
+# echo "S"
+# echo "T"
+# echo "U"
+# echo "V"
+# echo "W"
+# echo "X"
+# echo "Y"
+# echo "Z"
+
+echo "Use this to set the paths, append paths at end:"
+echo "export PATH=\$PATH:"
+
+echo "Existing PATH set to:"
+echo $PATH
+echo ""
