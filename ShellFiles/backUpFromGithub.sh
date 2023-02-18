3345caab3eb88bab60051dfb40ab5009e13c8639
Soumyajit Samanta <samanta.soumyajit@gmail.com>
2023-02-18 18:43:15 +0530

    Added backup from github script

---
+++ ShellFiles/backUpFromGithub.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/soumyajitsamanta/shastras.git
+cd shastras && git pull && cd ..
+git clone https://github.com/bvschaik/julius.git
+cd julius && git pull && cd ..
+git clone https://github.com/rochus-keller/LeanQt.git
+cd LeanQt && git pull && cd ..
+git clone https://github.com/Kong/insomnia.git
+cd insomnia && git pull && cd ..
+git clone https://github.com/SchemaStore/schemastore.git
+cd schemastore && git pull && cd ..
+echo "next"
