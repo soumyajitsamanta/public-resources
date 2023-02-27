214cab0df157e302140c0bb902531683e91997d6
soumyajitsamanta <samanta.soumyajit@gmail.com>
2023-02-27 12:31:56 +0530

    Corrected folder

--- ShellFiles/backUpFromGithub.sh
+++ ShellFiles/backUpFromGithub.sh
@@ -1,11 +1,11 @@
-cd ~/Checkout && git clone https://github.com/soumyajitsamanta/shastras.git
+cd ~/Project/Checkout && git clone https://github.com/soumyajitsamanta/shastras.git
 cd shastras && git pull && cd ..
-cd ~/Checkout && git clone https://github.com/bvschaik/julius.git
+cd ~/Project/Checkout && git clone https://github.com/bvschaik/julius.git
 cd julius && git pull && cd ..
-cd ~/Checkout && git clone https://github.com/rochus-keller/LeanQt.git
+cd ~/Project/Checkout && git clone https://github.com/rochus-keller/LeanQt.git
 cd LeanQt && git pull && cd ..
-cd ~/Checkout && git clone https://github.com/Kong/insomnia.git
+cd ~/Project/Checkout && git clone https://github.com/Kong/insomnia.git
 cd insomnia && git pull && cd ..
-cd ~/Checkout && git clone https://github.com/SchemaStore/schemastore.git
+cd ~/Project/Checkout && git clone https://github.com/SchemaStore/schemastore.git
 cd schemastore && git pull && cd ..
 echo "+ Complete"
