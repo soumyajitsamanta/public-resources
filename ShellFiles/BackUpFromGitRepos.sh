1b9d97a371c127f813edf82f7ab854ddb88534f0
Soumyajit Samanta <samanta.soumyajit@gmail.com>
2023-08-19 19:56:54 +0530

    Added SVN to Backup repositories script

--- ShellFiles/BackUpFromGitRepos.sh
+++ ShellFiles/BackUpFromGitRepos.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 # Ignored Repositories: rochus-keller/LeanQt.git
-GITHUB_DOMAIN=https://github.com
+CURRENT_DIR=$(pwd)
 echo "Checking out git repositories in GitRepos.json"
-# for REPO_LOC in JabRef/jabref soumyajitsamanta/shastras bvschaik/julius Kong/insomnia SchemaStore/schemastore ioquake/ioq3 apache/jmeter
-for REPO_LOC in $(cat GitRepos.json)
+for REPO_LOC in $(cat $CURRENT_DIR/GitRepos.json)
 do
-    echo $REPO_LOC
     cd ~/Project/Checkout
+    echo $REPO_LOC
     git clone $REPO_LOC
-    FOLDER_LOC=$(echo $REPO_LOC | sed "s/\.git//" | sed "s/^.*\///")
+    FOLDER_LOC=$(basename -s .git $REPO_LOC)
     cd $FOLDER_LOC
-        git pull
-    cd ..
+    git pull
 done
+
+echo "Checking out svn repositories in SvnReposDirectories.list"
+for REPO_LOC_DIR in $(cat $CURRENT_DIR/SvnReposDirectories.csv)
+do
+    cd ~/Project/Checkout
+    REPO_LOC=$(echo $REPO_LOC_DIR | awk -F "," '{print($1)}')
+    echo $REPO_LOC
+    FOLDER_LOC=$(echo $REPO_LOC_DIR | awk -F "," '{print($2)}')
+    svn checkout $REPO_LOC $FOLDER_LOC
+    cd $FOLDER_LOC
+    svn up
+done
+
+echo ""
 echo "+ Complete"
