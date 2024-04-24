cd781bf7549efc48d2484e2d3238341fe4aa28c1
Soumyajit Samanta <samanta.soumyajit@gmail.com>
2024-04-24 17:34:52 +0530

    Adding template for Maven projects

---
+++ Templates/JavaProjects/maven-project-template/child-project-template/src/main/java/pack/MainApplication.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Remove default template code");
+    }
+
+}
---
+++ Templates/JavaProjects/maven-project-template/child-project-template/src/test/java/pack/MainApplicationTest.java
@@ -0,0 +1,9 @@
+package pack;
+
+import org.junit.jupiter.api.Assertions;
+
+public class MainApplicationTest {
+    public static void main(String[] args) {
+        Assertions.assertTrue(1 == 2, "Remove Default Test");
+    }
+}
