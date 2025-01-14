7af3d5abbf5d73431543b79224a7ba90a831b4d7
Soumyajit Samanta <samanta.soumyajit@gmail.com>
2025-01-14 18:47:04 +0530

    Added git svn repos

--- ShellFiles/SvnReposDirectories.sh
+++ ShellFiles/SvnReposDirectories.sh
@@ -1,4 +1,7 @@
-svn checkout https://svn.code.sf.net/p/lipitk/code/ lipitk-code
-svn checkout https://svn.code.sf.net/p/hwrecogntool/code/ hwrecogntool-code
-svn checkout https://svn.code.sf.net/p/cssparser/code/ cssparser-code
-svn co http://svn.vuze.com/public/client/ vuze
+# svn co https://svn.code.sf.net/p/lipitk/code/ lipitk-code
+# svn co https://svn.code.sf.net/p/hwrecogntool/code/ hwrecogntool-code
+# svn co https://svn.code.sf.net/p/cssparser/code/ cssparser-code
+# svn co http://svn.vuze.com/public/client/ vuze
+# svn co https://svn.code.sf.net/p/xye/code xye-code
+# svn checkout https://svn.code.sf.net/p/buoy/code/ buoy-code
+svn co https://svn.apache.org/repos/asf/poi/trunk/ poi
